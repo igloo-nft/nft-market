cd9f34ba19c53bebbbed64af5ab9a32e2807b18e
Lawrence Nicastro <lawrence.nicastro1@gmail.com>
2021-08-29 23:41:00 -0400

    update webpack

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ module.exports = {
   webpack(config, { isServer }) {
     if (!isServer) {
       config.resolve.fallback.fs = false;
-      return config;
     }
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
     return config;
